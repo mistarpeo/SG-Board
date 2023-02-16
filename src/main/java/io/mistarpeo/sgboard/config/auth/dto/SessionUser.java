c9580262e4725a7fbd233ce9e3678370fa6ef68b
sgcho78 <mistarpeo@gmail.com>
2023-02-16 23:16:42 +0900

    spring security 적용 google oauth2 인증 추가

---
+++ src/main/java/io/mistarpeo/sgboard/config/auth/dto/SessionUser.java
@@ -0,0 +1,21 @@
+package io.mistarpeo.sgboard.config.auth.dto;
+
+import io.mistarpeo.sgboard.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
